5c137b9c3f926f8ada0e1f7357dbe86fcd7ff393
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-03-03 17:22:32 +0000

    first commit

---
+++ src/main/java/com/lifu/keycloak/Oauth2KeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.lifu.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2KeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2KeycloakApplication.class, args);
+    }
+
+}
